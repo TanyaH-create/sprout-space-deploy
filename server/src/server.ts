f6f5246e41e8f8e64c1489f6d8cc3fbed0c50751
Tanya <dougtanyah@gmail.com>
2025-04-03 13:40:41 -0400

    remove development variable code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,4 @@
-import express, {Request, Response } from "express";
-import path from "node:path";
-import { fileURLToPath } from 'url';
+import express from "express";
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
@@ -14,9 +12,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// Get the current directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const startApolloServer = async () => {
   await server.start();
@@ -35,13 +30,6 @@ app.use('/graphql', expressMiddleware(server as any,
   }
 ));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
