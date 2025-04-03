a5d8442d8535b59cfb86e69583772000d56f3a80
Tanya <dougtanyah@gmail.com>
2025-04-03 13:32:09 -0400

    remove deveopment from server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,4 @@
-import express, {Request, Response } from "express";
-import path from "node:path";
-import { fileURLToPath } from 'url';
+import express from "express";
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
@@ -15,8 +13,8 @@ const server = new ApolloServer({
 });
 
 // Get the current directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const startApolloServer = async () => {
   await server.start();
@@ -35,13 +33,13 @@ app.use('/graphql', expressMiddleware(server as any,
   }
 ));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
+  //   app.get('*', (_req: Request, res: Response) => {
+  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  //   });
+  // }
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
